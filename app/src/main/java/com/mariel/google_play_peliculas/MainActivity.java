7a1cc73fa55572436d277a937fe08d4335c118cc
Mariel Quispe <marielita.nil123@gmail.com>
2017-09-25 22:06:36 -0800

    Commit inicial

---
+++ app/src/main/java/com/mariel/google_play_peliculas/MainActivity.java
@@ -0,0 +1,15 @@
+package com.mariel.google_play_peliculas;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity
+{
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState)
+    {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
